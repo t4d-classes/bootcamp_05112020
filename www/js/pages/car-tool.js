d2c93980cfb1e58dcc06dbcd05ce6b200007b6bf
Eric Greene <eric@t4d.io>
2020-05-12 18:28:55 -0400

    add code demos

--- www/js/pages/car-tool.js
+++ www/js/pages/car-tool.js
@@ -1,3 +1,14 @@
+const config = {
+  columns: [
+      { caption: 'Id', field: 'id' },
+      { caption: 'Make', field: 'make' },
+      { caption: 'Model', field: 'model' },
+      { caption: 'Year', field: 'year' },
+      { caption: 'Color', field: 'color' },
+      { caption: 'Price', field: 'price' },
+  ],
+};
+
 const carRows = cars.map(car => {
   const trElement = document.createElement('tr');
   const carCols = config.columns.map(colConfig => {
