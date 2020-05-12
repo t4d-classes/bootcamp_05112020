5a1d388e800cbfe3b3f9a546f95a0ddd0f139d85
Eric Greene <eric@t4d.io>
2020-05-12 16:31:01 -0400

    add code demos

---
+++ www/demo/color-tool.js
@@ -0,0 +1,107 @@
+
+'use strict';
+
+// var t = 2;
+
+// let t = 2; // mutable variable
+// const t = 4; // immutable variable
+
+// console.log(t);
+// console.log(typeof t);
+
+// t = 'hello';
+
+// console.log(t);
+// console.log(typeof t);
+
+// t = true;
+
+// console.log(t);
+// console.log(typeof t);
+
+// const p = {
+//   firstName: 'Bob',
+//   lastName: 'Smith',
+//   address: {
+//     city: 'Mountain View',
+//     state: 'CA',
+//   },
+// };
+
+// Object.freeze(p);
+// Object.freeze(p.address);
+
+// p.age = 31;
+// delete p.lastName;
+// p.firstName = 'Tim';
+
+// p.address.city = 'Plano';
+
+
+// console.log(p);
+
+// // function declaration
+// function doIt() {
+//   console.log('do it');
+// }
+
+// doIt();
+
+// // function expression
+// const doIt2 = function() {
+//   console.log('do it 2');
+// };
+
+// console.log('test');
+
+// doIt2();
+
+// // arrow function
+// const doIt3 = () => {
+
+//   console.log('do it 3');
+
+// };
+
+// doIt3();
+
+
+// const doIt = (fn) => {
+//   console.log(fn(2, 3));
+// };
+
+
+// doIt((x, y) => x + y);
+
+// const nums = [1,2,3,4,5];
+
+// Array.prototype.map = function(transformFn) {
+
+//   console.log('run my hacked version');
+
+  // const newArray = [];
+
+  // for (let x=0; x<this.length; x++) {
+  //   newArray.push(transformFn(this[x]));
+  // }
+
+  // return newArray;
+
+// };
+
+
+
+// const doubleNums = nums.map((x) => {
+//   return x * 2;
+// });
+
+// console.log(nums);
+// console.log(doubleNums);
+
+
+const colors = [
+  { id: 1, name: 'red', hexcode: '#FF0000' },
+  { id: 2, name: 'blue', hexcode: '#0000FF' },
+];
+
+
