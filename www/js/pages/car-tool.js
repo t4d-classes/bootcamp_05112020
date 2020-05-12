3342b332701774d9c152aeb2d25e1dcab669c637
Eric Greene <eric@t4d.io>
2020-05-12 19:50:45 -0400

    add code demos

--- www/js/pages/car-tool.js
+++ www/js/pages/car-tool.js
@@ -1,7 +1,8 @@
 const config = {
+  
   columns: [
       { caption: 'Id', field: 'id' },
-      { caption: 'Make', field: 'make' },
+      { caption: 'Make', field: 'make', format: s => String(s).toUpperCase() },
       { caption: 'Model', field: 'model' },
       { caption: 'Year', field: 'year' },
       { caption: 'Color', field: 'color' },
@@ -9,14 +10,26 @@ const config = {
   ],
 };
 
-const carRows = cars.map(car => {
-  const trElement = document.createElement('tr');
-  const carCols = config.columns.map(colConfig => {
-      const tdElement = document.createElement('td');
-      tdElement.append(car[colConfig.field]);
-      return tdElement;
+
+const colors = [
+  { id: 1, name: 'red', hexcode: '#FF0000' },
+  { id: 2, name: 'blue', hexcode: '#0000FF' },
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const carRows = cars.map(car => {
+    const trElement = document.createElement('tr');
+    const carCols = config.columns.map(colConfig => {
+        const tdElement = document.createElement('td');
+        tdElement.append(colConfig.format ?
+          colConfig.format(car[colConfig.field]) : car[colConfig.field]);
+        return tdElement;
+    });
+    trElement.append(...carCols);
+    return trElement;
   });
-  trElement.append(...carCols);
-  return trElement;
+  
+  document.querySelector('#car-table > tbody').append(...carRows);
+
 });
-document.querySelector('#car-table > tbody').append(...carRows);
