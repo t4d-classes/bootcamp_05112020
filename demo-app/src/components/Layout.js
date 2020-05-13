649032eabd847ffcc522f99c95e2cb7f44a5bb66
Eric Greene <eric@t4d.io>
2020-05-13 14:22:00 -0400

    add code demos

--- demo-app/src/components/Layout.js
+++ demo-app/src/components/Layout.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
+import { ToolHeader } from './ToolHeader';
+
 import './Layout.css';
 
 export const Layout = () => {
 
   return <div className="page-layout">
 
-    <header id="page-header">
-      <h1>
-        <div>Cool Company, Inc.</div>
-      </h1>
-    </header>
+    <ToolHeader />
 
     {/*<footer id="page-footer">
       <small>&copy; 2020 Cool Company, Inc. All Rights Reserved.</small>
---
+++ demo-app/src/components/ToolHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ToolHeader = () => {
+
+  return (
+    <header id="page-header">
+      <h1>
+        <div>Cool Company, Inc.</div>
+      </h1>
+    </header>
+  );
+}
