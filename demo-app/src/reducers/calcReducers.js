3112ada8d08c2570e648a98f2688233e4079134a
Eric Greene <eric@t4d.io>
2020-05-15 20:30:26 -0400

    filter out init action

--- demo-app/src/reducers/calcReducers.js
+++ demo-app/src/reducers/calcReducers.js
@@ -20,8 +20,32 @@ export const resultReducer = (state = 0, action) => {
   }
 };
 
+const getOperator = (actionType) => {
+
+  switch (actionType) {
+    case ADD_ACTION:
+      return '+';
+    case SUBTRACT_ACTION:
+      return '-';
+    case MULTIPLY_ACTION:
+      return '*';
+    case DIVIDE_ACTION:
+      return '/';
+    default:
+      return '';
+  }
+
+};
+
 export const historyReducer = (state = [], action) => {
-  return state.concat({ opName: action.type, opValue: action.value });
+  if ([ADD_ACTION, SUBTRACT_ACTION, MULTIPLY_ACTION, DIVIDE_ACTION].includes(action.type)) {
+    return state.concat({
+      opName: getOperator(action.type),
+      opValue: action.value,
+    });
+  } else {
+    return state;
+  }
 };
 
 // export const calcReducer = (state = { }, action) => {
