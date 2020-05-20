02d2a50616a8fa0122d6d4f443e2e401c1b43763
Eric Greene <eric@t4d.io>
2020-05-20 17:09:30 -0400

    add demos

--- apollo-graphql-app-starter/graphql-server/resolvers.js
+++ apollo-graphql-app-starter/graphql-server/resolvers.js
@@ -9,25 +9,56 @@ export const resolvers = {
         .then(res => res.json());
     },
     color: (_, args, context) => {
-
       return fetch(`${context.restURL}/colors/${encodeURIComponent(args.colorId)}`)
         .then(res => res.json());
-
     },
     cars: (_1, _2, { restURL }) => {
       return fetch(`${restURL}/cars`)
         .then(res => res.json());
     },
+    car: (_, args, context) => {
+      return fetch(`${context.restURL}/cars/${encodeURIComponent(args.carId)}`)
+        .then(res => res.json());
+    },
   },
   Mutation: {
     appendColor: (_, args, context) => {
-
       return fetch(`${context.restURL}/colors`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(args.color),
       }).then(res => res.json());
+    },
+    appendCar: (_, args, context) => {
+      return fetch(`${context.restURL}/cars`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(args.car),
+      }).then(res => res.json());
+    },
+    replaceCar: async (_, args, context) => {
 
+      const res = await fetch(`${context.restURL}/cars/${encodeURIComponent(args.car.id)}`);
+      const car = await res.json();
+
+      await fetch(`${context.restURL}/cars/${encodeURIComponent(args.car.id)}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(args.car),
+      });
+
+      return car;
+    },
+    deleteCar: async (_, args, context) => {
+
+      return fetch(`${context.restURL}/cars/${encodeURIComponent(args.carId)}`)
+        .then(res => res.json())
+        .then(car => {
+          return fetch(
+            `${context.restURL}/cars/${encodeURIComponent(args.carId)}`,
+            { method: 'DELETE '},
+          ).then(() => car);
+        });
     },
   },
 };
--- apollo-graphql-app-starter/graphql-server/schema.js
+++ apollo-graphql-app-starter/graphql-server/schema.js
@@ -5,10 +5,14 @@ export const typeDefs = `
     colors: [Color]
     color(colorId: ID): Color
     cars: [Car]
+    car(carId: ID): Car
   }
 
   type Mutation {
     appendColor(color: AppendColor): Color
+    appendCar(car: AppendCar): Car
+    replaceCar(car: ReplaceCar): Car
+    deleteCar(carId: ID): Car
   }
 
   type Color {
@@ -30,4 +34,22 @@ export const typeDefs = `
     color: String
     price: Float
   }
+
+  input AppendCar {
+    make: String
+    model: String
+    year: Int
+    color: String
+    price: Float
+  }
+
+  input ReplaceCar {
+    id: ID
+    make: String
+    model: String
+    year: Int
+    color: String
+    price: Float
+  }
+
 `;
