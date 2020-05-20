e4bfff1588dc22eafcb1bc5862ba8952e1b3fdd4
Eric Greene <eric@t4d.io>
2020-05-20 13:29:03 -0400

    add code demos

--- apollo-graphql-app-starter/graphql-server/resolvers.js
+++ apollo-graphql-app-starter/graphql-server/resolvers.js
@@ -3,5 +3,12 @@ import fetch from 'node-fetch';
 export const resolvers = {
   Query: {
     message: () => 'Hello World!',
+    favoriteNumber: () => 8,
+    colors: (_1, _2, { restURL }) => {
+
+      return fetch(`${restURL}/colors`)
+        .then(res => res.json());
+
+    },
   },
 };
--- apollo-graphql-app-starter/graphql-server/schema.js
+++ apollo-graphql-app-starter/graphql-server/schema.js
@@ -1,5 +1,13 @@
 export const typeDefs = `
   type Query {
     message: String
+    favoriteNumber: Int
+    colors: [Color]
+  }
+
+  type Color {
+    id: ID
+    name: String
+    hexcode: String
   }
 `;
