2b5bc5a715d2158c95ec665451528e6c8bcee174
Eric Greene <eric@t4d.io>
2020-05-22 20:32:26 -0400

    add comments to saga unit test

--- demo-app/src/sagas/cars.test.js
+++ demo-app/src/sagas/cars.test.js
@@ -21,11 +21,21 @@ import {
 
 describe('Car Saga', () => {
 
+  // below are some comments for the refresh cars and how the iteration process works
+  // the code under test hits a yield, control returns to the unit test, where
+  // the value returned by the yield can be asseted
+
   test('Refresh Cars', () => {
+
+    // create the iterator from the generator function
     const iterator = refreshCars();
 
+    // trigger the call to the refresh cars API and
+    // expect the correct call functional invocation
+    // testing this code: const cars = yield call(refreshCarsAPI);
     expect(iterator.next().value).toEqual(call(refreshCarsAPI));
 
+    // the mock data to return
     const cars = [
       {
         id: 1,
@@ -37,9 +47,14 @@ describe('Car Saga', () => {
       },
     ];
 
+    // iterate to the code on the other side of the yield which
+    // dispatches the done action of the cars
+    // testing this code: yield put(createRefreshCarsDoneAction(cars));
     expect(iterator.next(cars).value).toEqual(
       put(createRefreshCarsDoneAction(cars)),
     );
+
+    // this process for this unit test is the same for all of the code below
 
   });
 
